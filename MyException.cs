b7a0325f5a1308d1557b6c6615b719c5b1b2210c
denis <106389581+fishman123456@users.noreply.github.com>
2022-11-09 17:16:41 +0700

    Initial commit

---
+++ MyException.cs
@@ -0,0 +1,13 @@
+﻿[Serializable]
+public class MyException : Exception
+{
+    public DateTime TimeExcept { get; private set; }
+    public MyException() : base("ошибка")
+    {
+        TimeExcept = DateTime.Now;
+    }
+    public MyException(string message) : base(message)
+    {
+        TimeExcept = DateTime.Now;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Создать свой класс исключения, выводящий сообщение об ошибке и текущую дату.
+// Использовать его в теле текущей программы:
+// Создан целочисленный массив на 10 чисел, рандомно его заполнить. +
+// При переборе массива, в случае если его значение чётное+
+// то вывести сообщение об ошибке. Использовать вложенные блоки try ---
+// также на проверку неправильного индекса массива.
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+
+int[] array = new int[10];
+Random random = new Random();
+
+for (int i = 0; i < 10; i++)
+{
+    try
+    {
+        array[i] = random.Next(1, 99);
+        int el_mass = array[i];
+        Console.Out.WriteLine("{0} ", el_mass);
+        if (el_mass % 2 == 0)
+           throw new MyException("делится без остатка");
+    }
+    catch (MyException e)
+    {
+        Console.WriteLine(e.TimeExcept);
+        Console.WriteLine(e.Message);
+    }
+}
